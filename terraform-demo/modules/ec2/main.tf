fc8e1b789af0963fdcb4777ea032501a67dd363a
Jeongjewww <gwmelody99@gmail.com>
2023-04-03 15:46:58 +0900

    update: ami data source name

--- terraform-demo/modules/ec2/main.tf
+++ terraform-demo/modules/ec2/main.tf
@@ -10,9 +10,9 @@ data "aws_key_pair" "demo_private" {
 
 #######################################
 # 최신 ami값 불러오기
-data "aws_ami" "tf-ami" {
-  most_recent      = true
-  owners           = ["amazon"]
+data "aws_ami" "demo_ami" {
+  most_recent = true
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
@@ -37,18 +37,18 @@ data "aws_ami" "tf-ami" {
 
 #######################################
 # bastion host on public subnet
-resource "aws_instance" "bastion-host" {
-  ami           = data.aws_ami.tf-ami.id
-  instance_type = var.instance_type
+resource "aws_instance" "bastion_host" {
+  ami                         = data.aws_ami.demo_ami.id
+  instance_type               = var.instance_type
   associate_public_ip_address = true
 
-#   vpc_security_group_ids = [
-#     var.bastion_sg
-#   ]
+  #   vpc_security_group_ids = [
+  #     var.bastion_sg
+  #   ]
 
-  availability_zone = "${element(var.azs, 0)}"
-  subnet_id = var.public_subnet[0]
-  key_name  = data.aws_key_pair.demo_public.key_name
+  availability_zone = element(var.azs, 0)
+  subnet_id         = var.public_subnet[0]
+  key_name          = data.aws_key_pair.demo_public.key_name
 
   root_block_device {
     volume_size = var.ebs_size
@@ -66,22 +66,22 @@ resource "aws_instance" "bastion-host" {
 
 #######################################
 # instances on private subnets
-resource "aws_instance" "demo-ec2" {
-  count         = "${length(var.azs)}"
-  ami           = data.aws_ami.tf-ami.id
+resource "aws_instance" "demo_ec2" {
+  count         = length(var.azs)
+  ami           = data.aws_ami.demo_ami.id
   instance_type = var.instance_type
-#   vpc_security_group_ids = [var.web_sg]
+  #   vpc_security_group_ids = [var.web_sg]
 
-  availability_zone = "${element(var.azs, count.index)}"
-  subnet_id = "${element(var.private_subnet, count.index)}"
-  key_name  = de
+  availability_zone = element(var.azs, count.index)
+  subnet_id         = element(var.private_subnet, count.index)
+  key_name          = data.aws_key_pair.demo_private.key_name
 
   root_block_device {
     volume_size = var.ebs_size
     volume_type = var.ebs_type
 
     tags = {
-      "Name" = "${var.env}-${var.tag}-private-volume-${count.index+1}"
+      "Name" = "${var.env}-${var.tag}-private-volume-${count.index + 1}"
     }
   }
 
