68af288f6ffcf76d70bcc5dc98179b4cdb623e28
Jeongjewww <gwmelody99@gmail.com>
2023-04-03 15:47:32 +0900

    update: network module format and outputs

--- terraform-demo/modules/network/main.tf
+++ terraform-demo/modules/network/main.tf
@@ -1,8 +1,8 @@
 #############################
 # vpc
 resource "aws_vpc" "demo_vpc" {
-  cidr_block       = "${var.vpc_cidr}"
-  enable_dns_support = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
@@ -13,26 +13,26 @@ resource "aws_vpc" "demo_vpc" {
 #################################
 # public subnet for bastion host
 resource "aws_subnet" "public_subnet" {
-  count = "${length(var.azs)}"
-  vpc_id = aws_vpc.demo_vpc.id
-  availability_zone = "${element(var.azs, count.index)}"
-  cidr_block = "${cidrsubnet(var.vpc_cidr, 8, count.index+1)}"
+  count                   = length(var.azs)
+  vpc_id                  = aws_vpc.demo_vpc.id
+  availability_zone       = element(var.azs, count.index)
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index + 1)
   map_public_ip_on_launch = true
-  
-  tags = { 
-    Name = "${var.env}-${var.tag}-pub-subnet${count.index+1}"
+
+  tags = {
+    Name = "${var.env}-${var.tag}-pub-subnet${count.index + 1}"
   }
 }
 
 # private subnet
 resource "aws_subnet" "private_subnet" {
-  count = "${length(var.azs)}"
-  vpc_id = aws_vpc.demo_vpc.id
-  availability_zone = "${element(var.azs, count.index)}"
-  cidr_block = "${cidrsubnet(var.vpc_cidr, 8, count.index+11)}"
-  
-  tags = { 
-    Name = "${var.env}-${var.tag}-pri-subnet${count.index+1}"
+  count             = length(var.azs)
+  vpc_id            = aws_vpc.demo_vpc.id
+  availability_zone = element(var.azs, count.index)
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 11)
+
+  tags = {
+    Name = "${var.env}-${var.tag}-pri-subnet${count.index + 1}"
   }
 }
 
@@ -49,7 +49,7 @@ resource "aws_internet_gateway" "demo_igw" {
 #############################
 # eip for ngw
 resource "aws_eip" "demo_eip" {
-  vpc      = true
+  vpc = true
   depends_on = [
     aws_internet_gateway.demo_igw
   ]
@@ -100,13 +100,13 @@ resource "aws_route_table" "demo_pri_rt" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = length(var.azs)
-  subnet_id = "${element(aws_subnet.public_subnet.*.id, count.index)}"
+  count          = length(var.azs)
+  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
   route_table_id = aws_route_table.demo_pub_rt.id
 }
 
 resource "aws_route_table_association" "web_private" {
-  count = "${length(var.azs)}"
-  subnet_id = "${element(aws_subnet.private_subnet.*.id, count.index)}"
+  count          = length(var.azs)
+  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
   route_table_id = aws_route_table.demo_pri_rt.id
 }
--- terraform-demo/modules/network/outputs.tf
+++ terraform-demo/modules/network/outputs.tf
@@ -1,21 +1,34 @@
 output "vpc_id" {
-    value = aws_vpc.demo_vpc.id
-    description = "id of vpc"
+  value       = aws_vpc.demo_vpc.id
+  description = "id of vpc"
 }
 
 output "vpc_cidr" {
-    value = aws_vpc.demo_vpc.cidr_block
-    description = "cidr block of vpc"
+  value       = aws_vpc.demo_vpc.cidr_block
+  description = "cidr block of vpc"
 }
 
 output "availability_zones" {
-  value = var.azs
+  value       = var.azs
+  description = "availability zones"
+}
+
+output "public_subnet_id" {
+  value       = aws_subnet.public_subnet[*].id
+  description = "id of public subnet"
 }
 
 output "public_subnet_cidr_block" {
-  value = aws_subnet.public_subnet[*].cidr_block
+  value       = aws_subnet.public_subnet[*].cidr_block
+  description = "cidr block of public subnet"
+}
+
+output "private_subnet_id" {
+  value       = aws_subnet.private_subnet[*].id
+  description = "id of private subnet"
 }
 
 output "private_subnet_cidr_block" {
-  value = aws_subnet.private_subnet[*].cidr_block
+  value       = aws_subnet.private_subnet[*].cidr_block
+  description = "cidr block of private subnet"
 }
