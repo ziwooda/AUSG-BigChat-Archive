eae112a2280bc46bf355bfb79795474ac1710803
Jeongjewww <gwmelody99@gmail.com>
2023-04-04 02:34:29 +0900

    update: tf formatting

--- terraform-demo/root/prod/main.tf
+++ terraform-demo/root/prod/main.tf
@@ -1,8 +1,8 @@
 # 각 모듈에 대하여 terraform.tfstate 저장 경로 동적 처리
 # terraform {
 #   backend "s3" {
-#     bucket = "terraform-remote-state-camezii"
-#     key    = "prod/terraform.tfstate"
+#     bucket         = "terraform-remote-state-camezii"
+#     key            = "prod/terraform.tfstate"
 #     region         = "ap-northeast-2"
 #     dynamodb_table = "TerraformStateLock"
 #     encrypt        = true
